4c480f4238ffc589e5c2b3e11c75e9362c518e0e
petr57 <ololo@gmail.com>
2015-05-17 16:09:13 +0300

    lab4: add tests template

---
+++ Lab4/src/Tests/CosineTest.java
@@ -0,0 +1,28 @@
+package Tests;
+
+/**
+ * Created by Petr on 17.05.2015.
+ */
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import junit.framework.Assert;
+
+public class CosineTest extends TestCase{
+    @Before
+    protected void setUp() throws Exception
+    {
+    }
+
+    @After
+    protected void tearDown() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testCosine() {
+
+    }
+}
---
+++ Lab4/src/Tests/TangentTest.java
@@ -0,0 +1,26 @@
+package Tests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by Petr on 17.05.2015.
+ */
+public class TangentTest {
+    @Before
+    protected void setUp() throws Exception
+    {
+    }
+
+    @After
+    protected void tearDown() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testTangent() {
+
+    }
+}
