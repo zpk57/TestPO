fa6818cb86d7baf185941e8b733dd368cb02ae09
Palmyr3 <palmur3@gmail.com>
2015-05-17 15:28:09 +0300

    arg check add

--- Lab4/src/Logarithm/NaturalLogarithm.java
+++ Lab4/src/Logarithm/NaturalLogarithm.java
@@ -5,12 +5,27 @@ package Logarithm;
 public class NaturalLogarithm
 {
     private long N = 25;
+    private static final double MAX_ARG = 3;
+    private static final double MIN_ARG = 2;
+    private static final double LN2 = 0.6931471806;
     public double ln(double arg)
     {
-        double result = 0;
-        for (long i = 1; i < N; i++)
+        double result;
+        if((arg >= MIN_ARG) && (arg <= MAX_ARG))
         {
-            result += Math.pow(-1,(i-1))*Math.pow((arg - 1), i)/i;
+//            for (long i = 1; i < N; i++)
+//            {
+//                result += Math.pow(-1, (i - 1)) * Math.pow((arg - 1), i) / i;
+//            }
+            result = LN2;
+            for (long i = 1; i < N; i++)
+            {
+                result += Math.pow((-1/2), (i - 2)) * Math.pow((arg - 1), i) / i;
+            }
+        }
+        else
+        {
+            result = Double.NaN;
         }
         return result;
     }
