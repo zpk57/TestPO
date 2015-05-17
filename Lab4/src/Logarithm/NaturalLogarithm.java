9f2aa3c69482bb221c62694b1614223ab13e8e07
Palmyr3 <palmur3@gmail.com>
2015-05-17 16:01:50 +0300

    bug fix

--- Lab4/src/Logarithm/NaturalLogarithm.java
+++ Lab4/src/Logarithm/NaturalLogarithm.java
@@ -4,10 +4,11 @@ package Logarithm;
 
 public class NaturalLogarithm
 {
-    private long N = 25;
+    private long N = 45;
     private static final double MAX_ARG = 3;
     private static final double MIN_ARG = 2;
     private static final double LN2 = 0.6931471806;
+    private static final double LN2d5 = 0.9162907319;
     public double ln(double arg)
     {
         double result;
@@ -17,10 +18,11 @@ public class NaturalLogarithm
 //            {
 //                result += Math.pow(-1, (i - 1)) * Math.pow((arg - 1), i) / i;
 //            }
+
             result = LN2;
             for (long i = 1; i < N; i++)
             {
-                result += Math.pow((-1/2), (i - 2)) * Math.pow((arg - 1), i) / i;
+                result -= Math.pow((((double)-1)/((double)2)), i) * Math.pow((arg - 2), i)/i;
             }
         }
         else
